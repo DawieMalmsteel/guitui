5a916c8127a26dba96593f0206fb6f14eebd9e63
dwcks <corclan19@gmail.com>
2025-12-30 01:42:20 +0700

    added catpucino theme

--- internal/theory/note.go
+++ internal/theory/note.go
@@ -20,19 +20,43 @@ const (
 	B
 )
 
+// catppuccin color palette
+var (
+	CatRosewater = lipgloss.Color("#f5e0dc")
+	CatFlamingo  = lipgloss.Color("#f2cdcd")
+	CatPink      = lipgloss.Color("#f5c2e7")
+	CatMauve     = lipgloss.Color("#cba6f7")
+	CatRed       = lipgloss.Color("#f38ba8")
+	CatMaroon    = lipgloss.Color("#eba0ac")
+	CatPeach     = lipgloss.Color("#fab387")
+	CatYellow    = lipgloss.Color("#f9e2af")
+	CatGreen     = lipgloss.Color("#a6e3a1")
+	CatTeal      = lipgloss.Color("#94e2d5")
+	CatSky       = lipgloss.Color("#89dceb")
+	CatSapphire  = lipgloss.Color("#74c7ec")
+	CatBlue      = lipgloss.Color("#89b4fa")
+	CatLavender  = lipgloss.Color("#b4befe")
+	CatText      = lipgloss.Color("#cdd6f4")
+	CatSubtext1  = lipgloss.Color("#bac2de")
+	CatOverlay1  = lipgloss.Color("#7f849c") // Màu xám cho dây đàn
+	CatSurface1  = lipgloss.Color("#45475a") // Màu nền nhẹ
+	CatBase      = lipgloss.Color("#1e1e2e") // Màu nền tối
+	CatCrust     = lipgloss.Color("#11111b") // Màu siêu tối
+)
+
 var NoteColors = map[Note]lipgloss.Color{
-	C:  lipgloss.Color("196"), // Red
-	Cs: lipgloss.Color("203"), // Light Red
-	D:  lipgloss.Color("208"), // Orange
-	Ds: lipgloss.Color("215"), // Light Orange
-	E:  lipgloss.Color("226"), // Yellow
-	F:  lipgloss.Color("46"),  // Green
-	Fs: lipgloss.Color("83"),  // Light Green
-	G:  lipgloss.Color("33"),  // Blue
-	Gs: lipgloss.Color("45"),  // Light Blue
-	A:  lipgloss.Color("129"), // Purple
-	As: lipgloss.Color("135"), // Light Purple
-	B:  lipgloss.Color("201"), // Pink
+	C:  CatRed,      // C = Đỏ pastel
+	Cs: CatMaroon,   // C# = Đỏ thẫm
+	D:  CatPeach,    // D = Cam đào
+	Ds: CatFlamingo, // D# = Hồng cam
+	E:  CatYellow,   // E = Vàng
+	F:  CatGreen,    // F = Xanh lá
+	Fs: CatTeal,     // F# = Xanh cổ vịt
+	G:  CatBlue,     // G = Xanh dương
+	Gs: CatSapphire, // G# = Xanh dương đậm
+	A:  CatMauve,    // A = Tím
+	As: CatLavender, // A# = Tím nhạt
+	B:  CatPink,     // B = Hồng
 }
 
 // Dùng cái này để hiển thị ra màn hình
